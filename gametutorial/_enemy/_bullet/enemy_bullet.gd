224ba7a47af14b6ca0b0eddca4bcfd7c8191d9be
W0rkingB0tz <156626644+W0rkingB0tz@users.noreply.github.com>
2025-01-26 21:21:13 -0700

    Update enemy_bullet.gd

--- gametutorial/_enemy/_bullet/enemy_bullet.gd
+++ gametutorial/_enemy/_bullet/enemy_bullet.gd
@@ -11,7 +11,6 @@ func _process(delta):
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
 
-
 func _on_area_entered(area: Area2D) -> void:
 	if area.name == "Player":
 		queue_free()
