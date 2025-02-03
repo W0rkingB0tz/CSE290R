ab817f4a1791023b87097a0199890562ff476b39
W0rkingB0tz <156626644+W0rkingB0tz@users.noreply.github.com>
2025-02-03 16:47:29 -0700

    Character Movement

--- abismo/Characters/_player/player.gd
+++ abismo/Characters/_player/player.gd
@@ -1,14 +1,12 @@
 extends CharacterBody2D
 
-@export var speed = 20000
-var score = 0
+@export var speed = 500
+
+func _ready():
+	speed = speed * 40
 
 func _process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * speed * delta
 	
 	move_and_slide()
-
-func update_score():
-	score += 1
-	print(score)
